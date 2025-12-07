f398a52259ad80f4b618b0362b7e6c8dd4e79e80
lszydlik <01187295@pw.edu.pl>
2025-12-07 23:43:37 +0100

    add menu services

---
+++ backend/menu/src/test/java/com/online_games_service/menu/MenuApplicationTests.java
@@ -0,0 +1,14 @@
+package com.online_games_service.menu;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest
+public class MenuApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
