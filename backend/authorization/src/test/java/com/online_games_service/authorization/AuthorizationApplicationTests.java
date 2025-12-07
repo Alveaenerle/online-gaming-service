a6e300c55d4fa436cc38c45e5252e90b77f5b7c8
lszydlik <xxstszydlik@gmail.com>
2025-12-07 23:17:00 +0100

    feat: split backend into services [OG-33]

---
+++ backend/authorization/src/test/java/com/online_games_service/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.online_games_service.authorization;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest
+public class AuthorizationApplicationTests extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ backend/social/src/test/java/com/online_games_service/social/SocialApplicationTests.java
@@ -0,0 +1,15 @@
+package com.online_games_service.social;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+
+@SpringBootTest
+public class SocialApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
