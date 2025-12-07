a1b8870bb57fa126f1c2def363de8fcb67f6036f
lszydlik <xxstszydlik@gmail.com>
2025-12-07 23:17:00 +0100

    split backend to microservices

---
+++ backend/tests/src/test/java/com/online_games_service/tests/TestsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.online_games_service.tests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class TestsApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
