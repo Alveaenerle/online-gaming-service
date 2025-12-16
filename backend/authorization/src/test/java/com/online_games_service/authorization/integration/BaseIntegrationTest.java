de70d6fc1e7ec6a221782fd2806a67bda7f420a1
Michał Iwanow-Kołakowski <michal2005ik@gmail.com>
2025-12-16 22:39:08 +0100

    Fixed BaseIntegrationTest.java

--- backend/authorization/src/test/java/com/online_games_service/authorization/integration/BaseIntegrationTest.java
+++ backend/authorization/src/test/java/com/online_games_service/authorization/integration/BaseIntegrationTest.java
@@ -1,9 +1,11 @@
 package com.online_games_service.authorization.integration;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
@@ -11,6 +13,7 @@ import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ContextConfiguration(initializers = BaseIntegrationTest.DockerInitializer.class) 
 public abstract class BaseIntegrationTest extends AbstractTestNGSpringContextTests {
 
     static final MongoDBContainer mongoDBContainer;
@@ -26,14 +29,19 @@ public abstract class BaseIntegrationTest extends AbstractTestNGSpringContextTes
         redisContainer.start();
     }
 
-    @DynamicPropertySource
-    static void dynamicProperties(DynamicPropertyRegistry registry) {
-        // Mongo URI
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-        
-        // Redis Host/Port/Pass
-        registry.add("spring.data.redis.host", redisContainer::getHost);
-        registry.add("spring.data.redis.port", redisContainer::getFirstMappedPort);
-        registry.add("spring.data.redis.password", () -> "testredispass");
+    static class DockerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            String mongoUri = mongoDBContainer.getReplicaSetUrl();
+            Integer redisPort = redisContainer.getFirstMappedPort();
+            String redisHost = redisContainer.getHost();
+
+            TestPropertyValues.of(
+                "spring.data.mongodb.uri=" + mongoUri,
+                "spring.data.redis.host=" + redisHost,
+                "spring.data.redis.port=" + redisPort,
+                "spring.data.redis.password=testredispass" 
+            ).applyTo(applicationContext.getEnvironment());
+        }
     }
 }
