0c6235a9381ff44fdf382d004a656d062a10bd5f
Michał Iwanow-Kołakowski <michal2005ik@gmail.com>
2025-12-16 21:10:43 +0100

    Fix to tests to pass Jenkins build

--- backend/authorization/src/test/java/com/online_games_service/authorization/AuthorizationApplicationTests.java
+++ backend/authorization/src/test/java/com/online_games_service/authorization/AuthorizationApplicationTests.java
@@ -2,9 +2,11 @@ package com.online_games_service.authorization;
 
 import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AuthorizationApplicationTests extends AbstractTestNGSpringContextTests {
 
     @Test
