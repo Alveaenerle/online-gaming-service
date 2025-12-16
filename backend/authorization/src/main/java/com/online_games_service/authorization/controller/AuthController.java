88e067fd5ec527625797d2f1ad92fc99fc1eec25
Michał Iwanow-Kołakowski <michal2005ik@gmail.com>
2025-12-16 01:19:26 +0100

    fix: Fixed value for cors in application.properties [OG-25]

--- backend/authorization/src/main/java/com/online_games_service/authorization/controller/AuthController.java
+++ backend/authorization/src/main/java/com/online_games_service/authorization/controller/AuthController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.online_games_service.authorization.dto.LoginRequest;
