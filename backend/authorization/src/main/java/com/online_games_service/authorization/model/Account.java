4a8ddcd790ef2b48e8159d1c06160815ebb613ee
m-ik05 <michal2005ik@gmail.com>
2025-12-16 01:23:16 +0100

    revert: "Revert "Feature/og 25" [OG-25] (#30)" [OG-25] (#31)
    * Revert "Revert "Feature/og 25" [OG-25] (#30)"
    
    This reverts commit 27639ae502a63ba5c013dff6b7838a04068494de.
    
    * fix: Replaced key in properties file with key generation, added logging, deleted redundant dependencies [OG-25]
    
    * fix: Fixed value for cors in application.properties [OG-25]

--- backend/authorization/src/main/java/com/online_games_service/authorization/model/Account.java
+++ backend/authorization/src/main/java/com/online_games_service/authorization/model/Account.java
@@ -2,6 +2,7 @@ package com.online_games_service.authorization.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -24,14 +25,24 @@ public class Account {
     @Indexed(unique = true)
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password cannot be blank")
     private String passwordHash;
+
+    @NotBlank(message = "User ID cannot be blank")
+    @Indexed(unique = true)
+    private String userId;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
 
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public Account(String email, String passwordHash) {
+    public Account(String email, String passwordHash, String userId, String username) {
         this.email = email;
         this.passwordHash = passwordHash;
+        this.userId = userId;
+        this.username = username;
     }
 }
