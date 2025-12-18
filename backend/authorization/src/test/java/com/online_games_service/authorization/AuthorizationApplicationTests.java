fb27ffdc776dbc0621bd09809cae04f653b37660
dominik <01197296@pw.edu.pl>
2025-12-18 01:11:20 +0100

    feat: improve backend testing

--- backend/authorization/src/test/java/com/online_games_service/authorization/AuthorizationApplicationTests.java
+++ backend/authorization/src/test/java/com/online_games_service/authorization/AuthorizationApplicationTests.java
@@ -1,9 +1,8 @@
 package com.online_games_service.authorization;
 
-import com.online_games_service.authorization.integration.BaseIntegrationTest;
 import org.testng.annotations.Test;
 
-public class AuthorizationApplicationTests extends BaseIntegrationTest {
+public class AuthorizationApplicationTests {
 
     @Test
     public void contextLoads() {
