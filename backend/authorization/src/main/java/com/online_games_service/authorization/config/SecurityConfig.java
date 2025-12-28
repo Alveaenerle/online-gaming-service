02b7ea2ae6b8e6b8160f7f5ee3e3843347bdd195
jszwagie <01187293@pw.edu.pl>
2025-12-28 16:14:33 +0100

    hide me endpoint from public

--- backend/authorization/src/main/java/com/online_games_service/authorization/config/SecurityConfig.java
+++ backend/authorization/src/main/java/com/online_games_service/authorization/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/register", "/login", "/guest", "/logout", "/me").permitAll()
+                .requestMatchers("/register", "/login", "/guest", "/logout").permitAll()
                 .anyRequest().authenticated()
             )
             .logout(logout -> logout.disable());
