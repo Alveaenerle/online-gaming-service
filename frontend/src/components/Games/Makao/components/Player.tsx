c2ed5f7e375031d2614b77c2f8e88b4eb43ddde3
jszwagie <01187293@pw.edu.pl>
2026-01-13 00:08:46 +0100

    feat: fix social component and center logo on makao

--- frontend/src/components/Games/Makao/components/Player.tsx
+++ frontend/src/components/Games/Makao/components/Player.tsx
@@ -25,7 +25,7 @@ const TurnTimerRing: React.FC<TurnTimerRingProps> = ({
 
   return (
     <svg
-      className="absolute inset-0 -rotate-90"
+      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-90"
       width={size}
       height={size}
       viewBox={`0 0 ${size} ${size}`}
